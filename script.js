cec2af6581ae95f0be3564cc77a0f08bb156275b
Steven Barrios <steven.barrios92@gmail.com>
2022-08-20 12:43:45 -0700

    readjust some variables and create if statement

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
 // Assignment code here
 var specialChar = ["!", "\u0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\u005C", "]", "^", "_", "`", "{", "|", "}", "~"];
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
@@ -7,20 +12,25 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var allArray = [specialChar, lowerCase, numeric, upperCase];
 
 function generatePassword() {
-  var userLength = confirm("Please select how many characters? \n 8 Minimum, 128 Maximum");
-  var userLower = confirm("How many lower case letters? \n Ok for Yes, Cancel for No");
-  var specialChar = confirm("How many special characters? \n Ok for Yes, Cancel for No");
-  var userSpecial = confirm("How many ? \n Ok for Yes, Cancel for No");
+  var password = " ";
+  var userLength = prompt("Please select how many characters? \n 8 Minimum, 128 Maximum");
+  var userLower = confirm("Would you like to use lower case letters? \n Ok for Yes, Cancel for No");
+  var specialChar = confirm("Would you like to use special characters? \n Ok for Yes, Cancel for No");
   var userCapital = confirm("How many capital letters? \n Ok for Yes, Cancel for No");
+  var numeric = confirm("Would you like to use numbers? \n Ok for yes, Cancel for No");
 
-  if (userLenght < 8 || userLength > 128) {
-    alert ("Please select a number between 8 and 128 \n for Password Length");
+  if ( userLength < 8 || userLength > 128) {
+   // alert ("Please select a number between 8 and 128 \n for Password Length");
+    prompt.userLength;
+    return alert("Please Select Between 8 and 128");
+  } else {
+    return password;
+  }
 
+  function password () {
+    var writePassword = [Math.floor(Math.random() * allArray.userLength)]
   }
 };
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
